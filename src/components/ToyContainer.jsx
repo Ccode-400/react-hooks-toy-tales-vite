8086c47d4c2c6e73a030439f88d52c6cd683a985
Ccode-400 <stanleymngige@gmail.com>
2026-05-12 05:15:47 +0300

    rendered ToyCard collection

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -3,7 +3,16 @@ import ToyCard from "./ToyCard";
 
 function ToyContainer({ toys, onDelete, onLike }) {
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{/* Render the collection of ToyCards */}
+    {toys.map((toy) => (
+        <ToyCard
+          key={toy.id}
+          toy={toy}
+          onDelete={onDelete}
+          onLike={onLike}
+        />
+      ))}
+    </div>
   );
 }
 
